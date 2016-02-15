ad27215939b93b878d37ca2606e426b8f46e141b
dmitry <dmitry.trok@gmail.com>
2016-02-15 21:02:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('hello world\n');
-}).listen(8000, '127.0.0.1');
+}).listen(80, '127.0.0.1');
